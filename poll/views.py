389676b4e7aa10e883677d1e08ddeba74fcd0f5b
tanner.cherry <tanner.cherry@gmail.com>
2016-10-12 13:37:12 -0500

    "nwe"

--- poll/views.py
+++ poll/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 import time
 # Create you views here.
 def main(request):
+    a = 1 + 2;
     return HttpResponse("Current date and time: " + time.strftime("%c"));
